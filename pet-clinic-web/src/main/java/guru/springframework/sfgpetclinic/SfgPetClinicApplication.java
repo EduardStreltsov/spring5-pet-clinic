565035b643d3bedde91dae654d250bf2944d91b3
eduard.streltsov <streltsov.eduard@gmail.com>
2018-08-17 22:12:40 +0300

    069. Implement Map Based Services 2

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
+	    System.out.println("\033[0;31m" + "something something" + "\033[0m");
         SpringApplication.run(SfgPetClinicApplication.class, args);
     }
 }
