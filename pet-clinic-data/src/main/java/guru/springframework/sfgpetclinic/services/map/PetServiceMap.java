43bfac0272cd15bdd655d0509510fc91635f861e
Eduard Streltsov <streltsov.eduard@gmail.com>
2018-09-03 23:05:13 +0300

    till lecture 70

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -1,24 +1,24 @@
 package guru.springframework.sfgpetclinic.services.map;
 
-import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.CrudService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
 	
 	@Override
-	public Set<Owner> findAll() {
+	public Set<Pet> findAll() {
 		return super.findAll();
 	}
 	
 	@Override
-	public Owner findById(Long id) {
+	public Pet findById(Long id) {
 		return super.findById(id);
 	}
 	
 	@Override
-	public Owner save(Owner object) {
+	public Pet save(Pet object) {
 		return super.save(object.getId(), object);
 	}
 	
@@ -28,7 +28,7 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 	}
 	
 	@Override
-	public void delete(Owner object) {
+	public void delete(Pet object) {
 		super.delete(object);
 	}
 
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,24 +1,24 @@
 package guru.springframework.sfgpetclinic.services.map;
 
-import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.CrudService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
 	
 	@Override
-	public Set<Owner> findAll() {
+	public Set<Vet> findAll() {
 		return super.findAll();
 	}
 	
 	@Override
-	public Owner findById(Long id) {
+	public Vet findById(Long id) {
 		return super.findById(id);
 	}
 	
 	@Override
-	public Owner save(Owner object) {
+	public Vet save(Vet object) {
 		return super.save(object.getId(), object);
 	}
 	
@@ -28,7 +28,7 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 	}
 	
 	@Override
-	public void delete(Owner object) {
+	public void delete(Vet object) {
 		super.delete(object);
 	}
 
