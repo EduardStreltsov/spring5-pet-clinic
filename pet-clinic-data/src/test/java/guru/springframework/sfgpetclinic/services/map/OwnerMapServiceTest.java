ebb61e4e6ed5e147ec5225165d39e2697288f96d
Eduard Streltsov <streltsov.eduard@gmail.com>
2018-09-27 21:54:57 +0300

    Lecture 191

--- pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/map/OwnerMapServiceTest.java
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/map/OwnerMapServiceTest.java
@@ -7,16 +7,19 @@ import org.junit.jupiter.api.Test;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class OwnerMapServiceTest {
 	
 	OwnerMapService ownerMapService;
+	Long ownerId1 = 1L;
+	Long ownerId2 = 2L;
 	
 	@BeforeEach
 	void setUp() {
 		ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
 		Owner owner = new Owner();
-		owner.setId(1L);
+		owner.setId(ownerId1);
 		ownerMapService.save(owner);
 	}
 	
@@ -28,10 +31,23 @@ class OwnerMapServiceTest {
 	
 	@Test
 	void findById() {
+		Owner owner = ownerMapService.findById(ownerId1);
+		assertEquals(ownerId1, owner.getId());
 	}
 	
 	@Test
-	void save() {
+	void saveExistingId() {
+		Long id = 2L;
+		Owner owner = new Owner();
+		owner.setId(id);
+		assertEquals(id, owner.getId());
+	}
+	
+	@Test
+	void saveNoId() {
+		Owner owner = ownerMapService(new Owner());
+		assertNotNull(owner);
+		assertNotNull(owner.getId());
 	}
 	
 	@Test
