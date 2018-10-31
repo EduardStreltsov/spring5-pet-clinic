f60059f80161a4e3fe2372b513e68a95e41d1365
eduard.streltsov <streltsov.eduard@gmail.com>
2018-10-31 20:40:59 +0200

    Lecture 192

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -2,11 +2,14 @@ package guru.springframework.sfgpetclinic.services.springdatajpa;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.repositories.PetTypeRepository;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -14,14 +17,23 @@ import java.util.Set;
 public class OwnerSDJpaService implements OwnerService {
 	
 	private final OwnerRepository ownerRepository;
+	private final PetRepository petRepository;
+	private final PetTypeRepository petTypeRepository;
 	
-	public OwnerSDJpaService(OwnerRepository ownerRepository) {
+	public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
 		this.ownerRepository = ownerRepository;
+		this.petRepository = petRepository;
+		this.petTypeRepository = petTypeRepository;
 	}
 	
 	@Override
 	public Owner findByLastName(String lastName) {
 		return ownerRepository.findByLastName(lastName);
+	}
+	
+	@Override
+	public List<Owner> findAllByLastNameLike(String lastName) {
+		return ownerRepository.findAllByLastNameLike(lastName);
 	}
 	
 	@Override
